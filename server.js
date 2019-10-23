86acd9359573222e3b9069d60d7c0d467990456a
Zaid <zaidjan1295@gmail.com>
2019-10-23 08:35:58 +0530

    fixed poert

--- server.js
+++ server.js
@@ -7,6 +7,8 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/index', express.static(path.join(__dirname, 'public')))
 // app.use('/analytics', express.static(path.join(__dirname, 'public'))
 
-app.listen(8000, ()=> {
-    console.log('app listening at port 8000');
+const port = process.env.PORT || 5001;
+
+app.listen(port, ()=> {
+    console.log(`app listening at port ${port}`);
 });
